a5f8cd6a075f06ac29441ce1a84bd6ffd1289f9e
Arthur Kayky Carvalho Silva <arthlokobr@gmail.com>
2026-07-22 23:49:20 -0300

    feat(schema): add products table

---
+++ src/database/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE products (
+    
+    product_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name
+    VARCHAR(256) NOT NULL, product_price DECIMAL(10, 2)
+    NOT NULL, product_url VARCHAR(512) NOT NULL, 
+
+    ); 
+
+CREATE TABLE product_categories (
+
+    product_model VARCHAR(256) NOT NULL,
+    product_sku VARCHAR(256) NOT NULL, product_manufacturer VARCHAR(256) NOT NULL, 
+    product_color VARCHAR(256) NOT NULL, product_category VARCHAR(256) NOT NULL, 
+
+);
