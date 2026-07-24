b624affd0b216c7b48bd40511c938e2ee2752b73
Arthur Kayky Carvalho Silva <arthlokobr@gmail.com>
2026-07-24 16:55:04 -0300

    refactor(schema): sql tables refactored

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,14 +1,42 @@
-CREATE TABLE IF NOT EXISTS products (
-    product_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    product_name VARCHAR(256) NOT NULL,
-    product_price DECIMAL(10,2) NOT NULL,
-    product_url VARCHAR(512) NOT NULL
+CREATE TABLE IF NOT EXISTS marketplace (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
 );
 
-CREATE TABLE IF NOT EXISTS product_categories (
-    product_model VARCHAR(256) NOT NULL,
-    product_sku VARCHAR(256) NOT NULL,
-    product_manufacturer VARCHAR(256) NOT NULL,
-    product_color VARCHAR(256) NOT NULL,
-    product_category VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS manufracturer (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS product (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(500) NOT NULL,
+    model VARCHAR(256) NOT NULL,
+    color VARCHAR(100) NOT NULL,
+    manufacturer_id INTEGER NOT NULL,
+    marketplace_id INTEGER NOT NULL,
+    FOREIGN KEY (manufacturer_id) REFERENCES manufracturer(id),
+    FOREIGN KEY (marketplace_id) REFERENCES marketplace(id)
+);
+
+CREATE TABLE IF NOT EXISTS product_price (
+    id INTEGER PRIMARY KEY,
+    product_id INTEGER NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    marketplace_id INTEGER NOT NULL,
+    FOREIGN KEY (marketplace_id) REFERENCES marketplace(id),
+    manufacturer_id INTEGER NOT NULL,
+    FOREIGN KEY (manufacturer_id) REFERENCES manufracturer(id)
+);
+
+CREATE TABLE IF NOT EXISTS urls (
+    id INTEGER PRIMARY KEY,
+    url VARCHAR(2500) NOT NULL,
+    sku VARCHAR(256) NOT NULL,
+    marketplace_id INTEGER NOT NULL,
+    FOREIGN KEY (marketplace_id) REFERENCES marketplace(id),
+    product_id INTEGER NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product(id)
 );
