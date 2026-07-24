dd653075734b71985428c1e950bc1a05f76aae37
Arthur Kayky Carvalho Silva <arthlokobr@gmail.com>
2026-07-24 18:44:15 -0300

    fix(schema): schema order fixed

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,42 +1,36 @@
 CREATE TABLE IF NOT EXISTS marketplace (
     id INTEGER PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
+    url VARCHAR(2500)
 );
 
-CREATE TABLE IF NOT EXISTS manufracturer (
+CREATE TABLE IF NOT EXISTS manufacturer (
     id INTEGER PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS product (
     id INTEGER PRIMARY KEY,
     name VARCHAR(500) NOT NULL,
     model VARCHAR(256) NOT NULL,
+    manufacturer VARCHAR(256) NOT NULL,
+    marketplace VARCHAR(256) NOT NULL,
     color VARCHAR(100) NOT NULL,
-    manufacturer_id INTEGER NOT NULL,
-    marketplace_id INTEGER NOT NULL,
-    FOREIGN KEY (manufacturer_id) REFERENCES manufracturer(id),
-    FOREIGN KEY (marketplace_id) REFERENCES marketplace(id)
+    category VARCHAR(500) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS product_price (
     id INTEGER PRIMARY KEY,
-    product_id INTEGER NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES product(id),
-    marketplace_id INTEGER NOT NULL,
-    FOREIGN KEY (marketplace_id) REFERENCES marketplace(id),
-    manufacturer_id INTEGER NOT NULL,
-    FOREIGN KEY (manufacturer_id) REFERENCES manufracturer(id)
+    manufacturer VARCHAR(256) NOT NULL,
+    marketplace VARCHAR(256) NOT NULL,
+    product_id INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS urls (
-    id INTEGER PRIMARY KEY,
-    url VARCHAR(2500) NOT NULL,
-    sku VARCHAR(256) NOT NULL,
-    marketplace_id INTEGER NOT NULL,
-    FOREIGN KEY (marketplace_id) REFERENCES marketplace(id),
-    product_id INTEGER NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product(id)
+CREATE TABLE IF NOT EXISTS product_url (
+    product_url VARCHAR(2500) NOT NULL,
+    marketplace_url VARCHAR(2500) NOT NULL,
+    product_sku VARCHAR(256) NOT NULL,
+    product_id INTEGER NOT NULL
 );
