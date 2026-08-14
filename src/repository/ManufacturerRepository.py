758b01ab6601e178388325a51cb83b3efa3c2620
TyrantDelet <arthlokobr@gmail.com>
2026-08-14 17:58:49 -0300

    refactor(Repository): implement correct methods for manufacturer info

--- src/repository/ManufacturerRepository.py
+++ src/repository/ManufacturerRepository.py
@@ -9,31 +9,35 @@ class ManufacturerRepository:
         self.db.connection.execute(query, (id, name, ))
         self.db.disconnect()
 
-    def get_manufacturer(self, id: int, name: str):
-        if id is not None and name is not None:
-            query = "SELECT * FROM manufacturer WHERE id = UPPER(?) and UPPER(name) = UPPER(?)"
-        elif id is not None:
-            query = "SELECT * FROM manufacturer WHERE id = ?"
-        elif name is not None:
-            query = "SELECT * FROM manufacturer WHERE name = ?"
-        else:
-            raise ValueError("Either 'id' or 'name' must be provided.")
-        result = self.db.connection.execute(query, (id or name,))
+    def get_manufacturer_by_id(self, id: int):
+        query = "SELECT * FROM manufacturer WHERE id = UPPER(?)"
+        result = self.db.connection.execute(query, (id,))
         self.db.disconnect()
-        return result if result else None
+        return result.fetchone() if result else None
+
+    def get_manufacturer_by_name(self, name: str):
+        query = "SELECT * FROM manufacturer WHERE UPPER(name) = UPPER(?)"
+        result = self.db.connection.execute(query, (name,))
+        self.db.disconnect()
+        return result.fetchone() if result else None
 
     def get_all_manufacturers(self):
         query = "SELECT * FROM manufacturer"
-        return self.db.fetchall(query)
+        return self.db.fetchone(query)
 
     def update_manufacturer(self, id: int, name: str):
-        query = "UPDATE manufacturer SET name = ? WHERE id = ?"
+        query = "UPDATE manufacturer SET name = UPPER(?) WHERE id = UPPER(?)"
         self.db.connection.execute(query, (name, id))
         self.db.disconnect()
 
-    def delete_manufacturer(self, id: int, name: str):
-        query = "DELETE FROM manufacturer WHERE id = ? AND name = ?"
-        self.db.connection.execute(query, (id, name))
+    def delete_manufacturer_by_id(self, id: int):
+        query = "DELETE FROM manufacturer WHERE id = UPPER(?)"
+        self.db.connection.execute(query, (id,))
+        self.db.disconnect()
+
+    def delete_manufacturer_by_name(self, name: str):
+        query = "DELETE FROM manufacturer WHERE UPPER(name) = UPPER(?)"
+        self.db.connection.execute(query, (name,))
         self.db.disconnect()
 
 
@@ -42,5 +46,5 @@ if __name__ == "__main__":
     manufacturer_repo = ManufacturerRepository(db)
 
     manufacturer_repo.add_manufacturer(1234, "ExampleManufacturer")
-    manufacturer = manufacturer_repo.get_manufacturer(1234, "ExampleManufacturer")
+    manufacturer = manufacturer_repo.get_manufacturer_by_id(1234)
     print(manufacturer)
