004790b5cdcc15e651e602f962e50f893d6b2700
TyrantDelet <arthlokobr@gmail.com>
2026-08-19 00:08:48 -0300

    feat(Repository): add fetchone abstraction method

--- src/repository/ManufacturerRepository.py
+++ src/repository/ManufacturerRepository.py
@@ -1,9 +1,21 @@
+import sqlite3
+
 from ..database.ManageDatabase import ManageDatabase
 
 class ManufacturerRepository:
     def __init__(self, db: ManageDatabase):
         self.db = db
+        self.connection = sqlite3.Connection
+        self.cursor: sqlite3.Cursor
 
+    def fetchone(self, query, params=None):
+            if params is None:
+                params = []
+            self.cursor.execute(query, params)
+            row = self.cursor.fetchone()
+            return dict(row) if row else None
+
+    
     def add_manufacturer(self, id: int, name: str):
         query = "INSERT INTO manufacturer (id, name) VALUES (?, ?)"
         self.db.connection.execute(query, (id, name, ))
@@ -48,3 +60,4 @@ if __name__ == "__main__":
     manufacturer_repo.add_manufacturer(1234, "ExampleManufacturer")
     manufacturer = manufacturer_repo.get_manufacturer_by_id(1234)
     print(manufacturer)
+    
