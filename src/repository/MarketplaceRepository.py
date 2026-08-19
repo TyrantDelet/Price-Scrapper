1be7371557f0d0c72798a89d773687a5d15dba5a
TyrantDelet <arthlokobr@gmail.com>
2026-08-19 00:10:06 -0300

    feat(Repository): add fetchone abstraction method

--- src/repository/MarketplaceRepository.py
+++ src/repository/MarketplaceRepository.py
@@ -1,9 +1,21 @@
+import sqlite3
+
 from ..database.ManageDatabase import ManageDatabase
 
 class MarketplaceRepository:
     def __init__(self, db: ManageDatabase):
         self.db = db
+        self.connection = sqlite3.Connection
+        self.cursor: sqlite3.Cursor
+    
+    def fetchone(self, query, params=None):
+            if params is None:
+                params = []
+            self.cursor.execute(query, params)
+            row = self.cursor.fetchone()
+            return dict(row) if row else None
 
+    
     def add_marketplace(self, id: int, name: str, url: str):
         query = "INSERT INTO marketplace (id, name, url) VALUES (?, ?, ?)"
         self.db.connection.execute(query, (id, name, url))
